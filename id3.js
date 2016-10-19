6daa4c4ac480ffc78133968024f6b8f93b937cf7
sswly <sswlymail@gmail.com>
2016-10-19 13:13:52 +0800

    Update id3.js

--- id3.js
+++ id3.js
@@ -1,3 +1,3 @@
 function getEntropy() {
-  return -(9/14)(Math.log(9/14)/Math.log(2))-(5/14)(Math.log(5/14)/Math.log(2));
+  return -(9/14)*(Math.log(9/14)/Math.log(2))-(5/14)*(Math.log(5/14)/Math.log(2));
 }
