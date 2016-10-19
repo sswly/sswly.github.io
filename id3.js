0b3618f643a259c55c93ab86b71c997349966ebd
sswly <sswlymail@gmail.com>
2016-10-19 16:02:22 +0800

    Update id3.js

--- id3.js
+++ id3.js
@@ -32,7 +32,7 @@ function getAttrData(sample, attr, attrStack) {
   var attrData = {"total":0, "data":{}};
   sample.forEach(function(item, index){
     if (!isValidData(item, attrStack)) {
-      //continue;
+      return;
     }
     
     if (attrData["data"][item[attr]] != undefined) {
