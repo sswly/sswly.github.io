b03dcda76e57b659182fae62f06e1ecdc4b58c55
sswly <sswlymail@gmail.com>
2016-10-21 22:29:06 +0800

    Update id3.js

--- id3.js
+++ id3.js
@@ -155,7 +155,7 @@ ID3 = {
 }
 
 function test() {
-  console.log("Version: 1.0.3.4");
+  console.log("Version: 1.0.3.5");
   console.log("Release: refactor with object");
 //   console.log("entropy=" + getEntropy(testSample, "Play ball", null));
 //   Object.keys(testSample[0]).forEach(function(item, index){
