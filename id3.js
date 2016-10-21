ed916e25094f9a406f039c436e258dfe3b1cc0c0
sswly <sswlymail@gmail.com>
2016-10-21 17:05:29 +0800

    Update id3.js

--- id3.js
+++ id3.js
@@ -66,91 +66,82 @@ SampleSet = {
   }
 }
 
-function getEntropy(sample, attr, attrStack) {
-  var attrData = SampleSet.count(sample, attr, attrStack);
-  var entropy = 0.0;
-
-  if (attrData["total"] <= 1) {
-    // all sample belong to same class
-    return entropy;
-  }
-  
-  Object.keys(attrData["data"]).forEach(function(item, index){
-    entropy += -(attrData["data"][item] / attrData["total"]) * (Math.log(attrData["data"][item] / attrData["total"]) / Math.log(2));
-  });
-  if (entropy == 0.0) {
-//     console.log("Decision branch: " + JSON.stringify(attrStack));
-  }
-  return entropy;
-}
-
-function getGain(sample, attr, attrGain, attrStack) {
-  var gain = getEntropy(sample, attr);
-  if (gain == 0.0) {
-    return gain;
-  }
-
-  var attrData = SampleSet.count(sample, attrGain, null);
-  if (attrData["total"] <= 1) {
-    return gain;
-  }
-  
-  Object.keys(attrData["data"]).forEach(function(item, index){
-    var newAttrStack = null;
-    if (attrStack == null) {
-      newAttrStack = [{"attr":attrGain, "value":item}];
-    } else {
-      newAttrStack = attrStack.concat({"attr":attrGain, "value":item});
-    }
-    gain -= (attrData["data"][item] / attrData["total"]) * getEntropy(sample, attr, newAttrStack);
-  });
-  return gain;
-}
-
-function getDecisionAttr(sample, attr, attrStack) {
-  if (getEntropy(sample, attr, attrStack) == 0) {
-    return null;
-  }
-  var decision = {"gain":0.0};
-  Object.keys(sample[0]).forEach(function(item, index){
-    if (attr == item || Sample.isValidAttr(item, attrStack)) {
-      return;
-    }
+ID3 = {
+  getEntropy: function(sampleSet, attr, constraint) {
+    var attrData = SampleSet.count(sampleSet, attr, constraint);
     
-    var gain = getGain(sample, attr, item, attrStack); 
-    if (gain > decision["gain"]) {
-      decision["gain"] = gain;
-      decision["attr"] = item;
-    }
-  });
-
-  return decision["attr"];
-}
-
-function genDecisionBranch(sample, attr, decisionTree) {
-  var decisionAttr = getDecisionAttr(sample, attr, decisionTree);
-  if (decisionAttr != null && decisionAttr != undefined) {
-    var attrData = SampleSet.count(sample, decisionAttr, decisionTree);
-    if (attrData["total"] <= 1) {
-      return;
-    }
+    var entropy = 0.0;
     Object.keys(attrData["data"]).forEach(function(item, index){
-      var newDecisionTree = null;
-      if (decisionTree == null) {
-        newDecisionTree = [{"attr":decisionAttr, "value":item}];
-      } else {
-        newDecisionTree = decisionTree.concat({"attr":decisionAttr, "value":item});
-      }
-      genDecisionBranch(sample, attr, newDecisionTree);
+      entropy += -(attrData["data"][item] / attrData["total"]) 
+        * (Math.log(attrData["data"][item] / attrData["total"]) / Math.log(2));
     });
-  } else {
-    attrData = SampleSet.count(sample, attr, decisionTree)
-    console.log("Decision branch: " + JSON.stringify(decisionTree) + "=>" + Object.keys(attrData["data"])[0]);
-  }
+    return entropy;
+  },
+  
+  getGain: function(sampleSet, attr, attrGain, constraint) {
+    var gain = getEntropy(sampleSet, attr);
+    if (gain == 0.0) {
+      return gain;
+    }
+
+    var attrData = SampleSet.count(sampleSet, attrGain, null);
+    Object.keys(attrData["data"]).forEach(function(item, index){
+      var newConstraint = null;
+      if (constraint == null) {
+        newConstraint = [{"attr":attrGain, "value":item}];
+      } else {
+        newConstraint = constraint.concat({"attr":attrGain, "value":item});
+      }
+      gain -= (attrData["data"][item] / attrData["total"]) * getEntropy(sampleSet, attr, newConstraint);
+    });
+    return gain;
+  },
+  
+  getDecisionNode: function(sample, attr, constraint) {
+    if (getEntropy(sample, attr, constraint) == 0) {
+      return null;
+    }
+    var decision = {"gain":0.0};
+    Object.keys(sample[0]).forEach(function(item, index){
+      if (attr == item || Sample.isValidAttr(item, constraint)) {
+        return;
+      }
+
+      var gain = getGain(sample, attr, item, constraint); 
+      if (gain > decision["gain"]) {
+        decision["gain"] = gain;
+        decision["attr"] = item;
+      }
+    });
+
+    return decision["attr"];
+  },
+
+  genDecisionTree: function(sample, attr, decisionTree) {
+    var decisionAttr = getDecisionNode(sample, attr, decisionTree);
+    if (decisionAttr != null && decisionAttr != undefined) {
+      var attrData = SampleSet.count(sample, decisionAttr, decisionTree);
+      if (attrData["total"] <= 1) {
+        return;
+      }
+      Object.keys(attrData["data"]).forEach(function(item, index){
+        var newDecisionTree = null;
+        if (decisionTree == null) {
+          newDecisionTree = [{"attr":decisionAttr, "value":item}];
+        } else {
+          newDecisionTree = decisionTree.concat({"attr":decisionAttr, "value":item});
+        }
+        genDecisionTree(sample, attr, newDecisionTree);
+      });
+    } else {
+      attrData = SampleSet.count(sample, attr, decisionTree)
+      console.log("Decision branch: " + JSON.stringify(decisionTree) + "=>" + Object.keys(attrData["data"])[0]);
+    }
+  },
 }
 
 function test() {
-  console.log("Version: 1.0.2.2");
+  console.log("Version: 1.0.2.3");
   console.log("Release: optimized and need to show better");
 //   console.log("entropy=" + getEntropy(testSample, "Play ball", null));
 //   Object.keys(testSample[0]).forEach(function(item, index){
@@ -161,5 +152,5 @@ function test() {
 //   console.log("getEntropy=" + getEntropy(testSample, "Play ball", [{"attr":"Outlook", "value":"Overcast"}]));
 //   console.log("getEntropy=" + getEntropy(testSample, "Play ball", [{"attr":"Outlook", "value":"Sunny"}, {"attr":"Humidity", "value":"High"}]));
 //   console.log("getDecisionAttr=" + getDecisionAttr(testSample, "Play ball", [{"attr":"Outlook", "value":"Rain"}]));
-  genDecisionBranch(testSample, "Play ball", null);
+  ID3.genDecisionTree(testSample, "Play ball", null);
 }
