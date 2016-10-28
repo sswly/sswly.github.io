e210510962a07997582bf6040c3499951c8d9aa2
sswly <sswlymail@gmail.com>
2016-10-28 15:56:27 +0800

    Update id3.js

--- id3.js
+++ id3.js
@@ -163,6 +163,7 @@ ID3 = {
       attrData = SampleSet.count(sample, attr, constraint);
       var leaf = Object.keys(attrData["data"])[0];
       ID3.decisionTree.nodes.push({id: from + "-" + leaf, label: leaf, shape: "triangleDown"});
+      if (constraint != null)
       ID3.decisionTree.edges.push({from: from, to: from + "-" + leaf, label: constraint[constraint.length - 1]["value"]});
       console.log("Decision branch: " + JSON.stringify(constraint) + "=>" + leaf);
       ID3.decisionBrachs.push(constraint.concat({"attr":attr, "value":leaf}));
