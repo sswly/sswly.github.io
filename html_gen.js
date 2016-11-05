c2d8501671525faab14a6de6fdc10abd29d28f20
sswly <sswlymail@gmail.com>
2016-11-05 20:36:47 +0800

    Update html_gen.js

--- html_gen.js
+++ html_gen.js
@@ -1,22 +1,24 @@
 HtmlTable = {
-  tableHead : [],
-  addTableHead: function(head) {
-    HtmlTable.tableHead.push(head);
-  },
-  
-  addTableHeadBeforeLast: function(head) {
-    HtmlTable.tableHead.splice(-1, 0, head);
-  },
-  
-  clearTableHead: function() {
-    HtmlTable.tableHead = [];
-  },
-  
   generate: function(id, data) {
+    var tableHead = [];
+    var isFirstData = true;
+    data.forEach(function(item, index){
+      Object.keys(item).forEach(function(attr, index){  
+        if (tableHead.indexOf(attr) == -1) {
+          if (isFirstData) {
+            tableHead.push(attr);
+          } else {
+            tableHead.splice(-1, 0, attr);
+          }
+        }
+      });
+      isFirstData = false;
+    });
+    
     var htmlTable = "<table id='" + id + "'>";
     //head
     htmlTable += "<tr>";
-    HtmlTable.tableHead.forEach(function(item, index){
+    tableHead.forEach(function(item, index){
       htmlTable += "<th>" + item + "</th>";
     });
     htmlTable += "</tr>";
@@ -24,7 +26,7 @@ HtmlTable = {
     //body
     data.forEach(function(row, index){
       htmlTable += "<tr>";
-      HtmlTable.tableHead.forEach(function(cell, index){  
+      tableHead.forEach(function(cell, index){  
         htmlTable += "<td>" + row[cell] + "</td>";
       });
       htmlTable += "</tr>";
