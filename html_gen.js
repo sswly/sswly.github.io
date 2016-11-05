d6c207ed5a034d67d9dceed11a53c80d1ae4c419
sswly <sswlymail@gmail.com>
2016-11-05 20:17:38 +0800

    Update html_gen.js

--- html_gen.js
+++ html_gen.js
@@ -1,1 +1,31 @@
-
+HtmlTable = {
+  tableHead : [],
+  addTableHead: function(head) {
+    HtmlTable.tableHead.push(head);
+  },
+  
+  addTableHeadBeforeLast: function(head) {
+    HtmlTable.tableHead.splice(-1, 0, head);
+  },
+  
+  generate: function(id, data) {
+    var htmlTable = "<table id='" + id + "'>";
+    //head
+    htmlTable += "<tr>";
+    HtmlTable.tableHead.forEach(function(item, index){
+      htmlTable += "<th>" + item + "</th>";
+    });
+    htmlTable += "</tr>";
+    
+    //body
+    data.forEach(function(row, index){
+      htmlTable += "<tr>";
+      HtmlTable.tableHead.forEach(function(cell, index){  
+        htmlTable += "<td>" + row[cell] + "</td>";
+      });
+      htmlTable += "</tr>";
+    });
+    htmlTable += "</table>";
+    return htmlTable;
+  },
+}
